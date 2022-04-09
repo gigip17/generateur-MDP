7beeb2892edd69d16a8a3111ed193c2ac8b4796a
gigip17 <70454351+gigip17@users.noreply.github.com>
2022-04-09 15:56:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "01234567890"
+const dataSymbols = "$_-^&é";
+
+function generatePassword() {
+    let data = []
+    if (lowercase.checked) data.push(dataLowercase);
+    if (lowercase.checked) data.push(dataUppercase);
+    if (lowercase.checked) data.push(dataNumbers);
+    if (lowercase.checked) data.push(dataSymbols);
+    
+    console.log(data);
+    
+}
+
+generateButton.assEventListener('click',generatePassword)
